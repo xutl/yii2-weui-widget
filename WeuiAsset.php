1f34b53350818c7aefa55b5afdcfbdc13ca0a96b
xuchao <85825770@qq.com>
2017-07-11 14:56:00 +0800

    init

---
+++ WeuiAsset.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * @link http://www.tintsoft.com/
+ * @copyright Copyright (c) 2012 TintSoft Technology Co. Ltd.
+ * @license http://www.tintsoft.com/license/
+ */
+
+namespace xutl\weui;
+
+use yii\web\AssetBundle;
+
+class WeuiAsset extends AssetBundle
+{
+    /**
+     * @var array list of CSS files that this bundle contains. Each CSS file can be specified
+     * in one of the three formats as explained in [[js]].
+     */
+    public $css = [
+        '//res.wx.qq.com/open/libs/weui/1.1.2/weui.min.css',
+    ];
+}
